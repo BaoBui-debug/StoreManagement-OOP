5edc7c409eafa0d912bb1413e41a3e4ccd7021a0
BaoBui-debug <schopfer.ac12@gmail.com>
2024-04-18 09:37:09 +0700

    Added Import delete page

---
+++ OOP/Pages/import/delete.cshtml.cs
@@ -0,0 +1,18 @@
+﻿using Presentation.Controllers;
+using Entity;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Presentation.Pages.import
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly ImportController _Controller = new();
+        public string Message = "Hóa đơn này sẽ bị xóa vĩnh viễn";
+        public void OnPost()
+        {
+            int index = int.Parse(Request.Query["id"].ToString());
+            _Controller.HandleRemove(index);
+            Response.Redirect("/view?i=ip");
+        }
+    }
+}
