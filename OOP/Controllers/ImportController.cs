7c0e6e1391a1c53fc35f242dc0789e20b83640df
BaoBui-debug <schopfer.ac12@gmail.com>
2024-04-18 09:25:52 +0700

    Fixed a bug

--- OOP/Controllers/ImportController.cs
+++ OOP/Controllers/ImportController.cs
@@ -44,8 +44,11 @@ namespace Presentation.Controllers
             foreach(Import i in iList)
             {
                 Product? item = pList.Find(p => p.GetIdentifier() == i.GetIdentifier());
-                i.Alive = item != null;
-                HandleUpdate(i, iList.IndexOf(i));
+                if(i.Alive)
+                {
+                    i.Alive = item != null;
+                    HandleUpdate(i, iList.IndexOf(i));
+                }
             }
             return iList;
         }
