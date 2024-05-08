f3be1ef4d9b744919de0c618b4a7ecd1a91fa3dd
BaoBui-debug <schopfer.ac12@gmail.com>
2024-05-08 13:25:03 +0700

    Added logout page

--- OOP/Pages/account/login.cshtml.cs
+++ OOP/Pages/account/login.cshtml.cs
@@ -7,10 +7,24 @@ namespace Presentation.Pages.account
 {
     public class LoginModel : PageModel
     {
-        private static readonly AccountController _Controller = new();
-        private static readonly AccountValidator _Validator = new(_Controller.FilePath);
+        private static readonly AccountController _AccountController = new();
+        private static readonly AccountValidator _Validator = new(_AccountController.FilePath);
         public string? FeedBack;
-        public string Message = "Bạn phải đăng nhập để sử dụng ứng dụng này";
+        public string? Message;
+        public bool IsNotLoggedin;
+        public void OnGet()
+        {
+            if (!_AccountController.ExistsUser(HttpContext.Session.GetString("username")))
+            {
+                IsNotLoggedin = true;
+                Message = "Hãy đăng nhập để sử dụng ứng dụng";
+            }
+            else
+            {
+                IsNotLoggedin = false;
+                Message = "Bạn đã đăng nhập vào hệ thống rồi";
+            }
+        }
         public void OnPost() 
         {
             string Name = Request.Form["name"].ToString();
---
+++ OOP/Pages/account/logout.cshtml.cs
@@ -0,0 +1,30 @@
+﻿using Presentation.Controllers;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Presentation.Pages.account
+{
+    public class LogoutModel : PageModel
+    {
+        private static readonly AccountController _AccountController = new();
+        public bool IsNotLoggedin;
+        public string? Message;
+        public void OnGet()
+        {
+            if (!_AccountController.ExistsUser(HttpContext.Session.GetString("username")))
+            {
+                IsNotLoggedin = true;
+                Message = "Bạn chưa đăng nhập vào hệ thống";
+            }
+            else
+            {
+                IsNotLoggedin = false;
+                Message = "Đăng xuất khỏi hệ thống?";
+            }
+        }
+        public void OnPost() 
+        {
+            HttpContext.Session.Remove("username");
+            Response.Redirect("/index");
+        }
+    }
+}
